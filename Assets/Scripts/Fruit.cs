014cbbe786cdae3ce3e2e5566dc3e5182a507fe9
rgsk <guptarahul70322@gmail.com>
2023-01-29 12:22:37 +0530

    fruits added

--- Assets/Scripts/Fruit.cs
+++ Assets/Scripts/Fruit.cs
@@ -1,18 +1,36 @@
-using System.Collections;
-using System.Collections.Generic;
+
 using UnityEngine;
 
-public class Fruit : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+public class Fruit : MonoBehaviour {
+    public GameObject whole;
+    public GameObject sliced;
+    private Rigidbody fruitRigidbody;
+    private Collider fruitCollider;
+    private ParticleSystem juiceParticleEffect;
+
+    private void Awake() {
+        fruitRigidbody = GetComponent<Rigidbody>();
+        fruitCollider = GetComponent<Collider>();
+        juiceParticleEffect = GetComponentInChildren<ParticleSystem>();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    private void Slice(Vector3 direction, Vector3 position, float force) {
+        whole.SetActive(false);
+        sliced.SetActive(true);
+        fruitCollider.enabled = false;
+        juiceParticleEffect.Play();
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        sliced.transform.rotation = Quaternion.Euler(0f, 0f, angle);
+        Rigidbody[] slices = sliced.GetComponentsInChildren<Rigidbody>();
+        foreach (var slice in slices) {
+            slice.velocity = fruitRigidbody.velocity;
+            slice.AddForceAtPosition(direction * force, position, ForceMode.Impulse);
+        }
+    }
+    private void OnTriggerEnter(Collider other) {
+        if (other.CompareTag("Player")) {
+            Blade blade = other.GetComponent<Blade>();
+            Slice(blade.direction, blade.transform.position, blade.sliceForce);
+        }
     }
 }
